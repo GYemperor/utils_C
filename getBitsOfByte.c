1a65e2bb5fe2ce7b9a33fa33ed3763adf9fa710a
wangguangyao2 <wangguangyao2@foton.com.cn>
2019-12-03 08:27:00 +0800

    提交getBitsOfByte

---
+++ getBitsOfByte.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int getBitsOfByte(int startPlace, int len, int data) {
+    int i = 0;
+    int testData = 1;
+    
+    if (startPlace + len > 7)
+    {
+        return -1;
+    }
+    
+
+    for ( i = 0; i < startPlace + len -1; i++)
+    {
+        testData <<= 1;
+        testData |= 1;
+    }
+    data &= testData;
+    data >>= startPlace;
+    return data;
+}
+
+int main() {
+    int data = getBitsOfByte(2,3,0b00011011);
+    printf("%d\n",data);
+}
