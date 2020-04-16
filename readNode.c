5f41903e9776f0611ce4a8a066c2a54d9a391747
wangguangyao2 <wangguangyao2@foton.com.cn>
2020-04-16 11:13:02 +0800

    read write节点整型数

--- readNode.c
+++ readNode.c
@@ -10,7 +10,7 @@ int main(){
     FILE *fp = NULL;
     char buf[1024] = {0};
     char tmp[8] = {0};
-    int fd = open("/sys/module/tbox_get/parameters/otaworkflag",O_RDONLY);
+    int fd = open("/sys/module/tbox_get/parameters/vehicle_name",O_RDONLY);
     if (fd > 0){
         printf("open %d\n",fd);
     }
@@ -18,18 +18,14 @@ int main(){
     close(fd);
     printf("open file and read:%s\n",tmp);
     printf("%#X\n",tmp[0]);
+    printf("%#X\n",tmp[1]);
+    printf("%#X\n",tmp[2]);
+    printf("%#X\n",tmp[3]);
+    printf("%#X\n",tmp[4]);
+    printf("%#X\n",tmp[5]);
+    printf("%#X\n",tmp[6]);
+    printf("%#X\n",tmp[7]);
 
-    fp = popen("cat /sys/module/tbox_get/parameters/otaworkflag","r");
-    printf("popen %d\n",*fp);
-    if (fp == NULL)
-    {
-        printf("popen faield\n");
-        return 0;
-    }
-    fread(buf, 1024,1,fp);
-    printf("%s\n",buf);
-    printf("%#X\n",buf[0]);
-    pclose(fp);
 
     return 0;
 }
--- writeNode.c
+++ writeNode.c
@@ -11,46 +11,34 @@ int main() {
   int fd;
   //timeval time_Now;
   int a[2] = {5,6};
-  char buffer[16];
+  char buffer[32];
   memset(buffer,30,sizeof(buffer));
-  char buffer_H[8];
-  char buffer_L[8];
 
   char otaworkflag[8];
   unsigned int tmp_otaworkflag = 1;
   sprintf(otaworkflag,"%u",tmp_otaworkflag);
 
-  unsigned long long tmp = 4294967295ULL;
+  //unsigned long long tmp = 4294967295ULL;
+  unsigned long long tmp = 4294967299ULL;
   printf("size of long:%d\n",sizeof(tmp));
   sprintf(buffer,"%u",tmp);
   printf("buffer:%s\n",buffer);
-  memcpy(buffer_L,&buffer[8],8);
-  memcpy(buffer_H,buffer,8);
-  printf("buffer_L:%#X\n",buffer_L);
-  printf("buffer_L:%s\n",buffer_L);
-  printf("buffer_H:%#X\n",buffer_H);
-  printf("buffer_H:%s\n",buffer_H);
-  fd = open("/sys/module/tbox_set/parameters/otaworkflag", O_WRONLY);
-  if (fd > 0)
-  {
-    printf("write fd = %d\n",fd);
-  }
-  write (fd, otaworkflag,sizeof(otaworkflag));
-  close(fd);
+
+
   fd = open("/sys/module/tbox_set/parameters/vehontime_h", O_WRONLY);
   if (fd > 0)
   {
     printf("write fd = %d\n",fd);
   }
-  write (fd, buffer_H,sizeof(buffer_H));
+  write (fd, buffer,sizeof(buffer));
   close(fd);
-  fd = open("/sys/module/tbox_set/parameters/vehontime_l", O_WRONLY);
-  if (fd > 0)
-  {
-    printf("write fd = %d\n",fd);
-  }
-  write (fd, buffer_L,sizeof(buffer_L));
-  close(fd);
+  // fd = open("/sys/module/tbox_set/parameters/vehontime_l", O_WRONLY);
+  // if (fd > 0)
+  // {
+  //   printf("write fd = %d\n",fd);
+  // }
+  // write (fd, buffer_L,sizeof(buffer_L));
+  // close(fd);
 
   return 0;
 }
